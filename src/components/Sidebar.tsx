d35f206fa7dfdabd59bc14fba66e3df81a200855
augustorl <gutin22@gmail.com>
2021-06-10 00:01:06 -0300

    add: Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text, Icon, Input, HStack, Box, Avatar} from '@chakra-ui/react'
+
+export function Sidebar() {
+    return (
+        <Flex
+        >
+    
+        </Flex>
+
+    )
+}
