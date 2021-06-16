60b89d1d394a30befee3a5b559b32a8e9c2a8097
augustorl <gutin22@gmail.com>
2021-06-16 19:55:42 -0300

    Refactor: Sidebar to components

---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,16 @@
+
+import { Link, Icon, Text, LinkProps} from "@chakra-ui/react";
+import React, { ElementType } from "react";
+
+interface NavLinkProps extends LinkProps {
+    icon: ElementType;
+    children: string;
+}
+export function NavLink({ icon, children, ...rest }) {
+    return (
+        <Link display="flex" align="center" {... rest}>
+            <Icon as={icon} fontSize="20" />
+            <Text ml="4" fontWeight="medium">{children}</Text>
+        </Link>
+    )
+}
