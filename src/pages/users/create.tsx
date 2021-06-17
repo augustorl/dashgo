5560dfacb9a5a3effe1518dce02d49b4ff2dc526
augustorl <gutin22@gmail.com>
2021-06-16 23:33:21 -0300

    update: responsive login

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -19,17 +19,17 @@ export default function UserCreate() {
                     flex="1"
                     borderRadius={8}
                     bg="gray.800"
-                    p="8"
+                    p={["6","8"]}
                 >
                     <Heading size="lg" fontWeight="normal" >Criar usuário</Heading>
                     <Divider my="6" borderColor="gray.700" />
 
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
                             <Input name="name" label="Nome Completo"/>
                             <Input name="email" label="E-mail"/>
                         </SimpleGrid>
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
                             <Input name="password" type="password" label="Senha"/>
                             <Input name="password_confirm" type="password" label="Confirmar senha"/>
                         </SimpleGrid>
