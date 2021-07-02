8603876f22e3c0eae6cd0db8fdac56259e6f9a59
augustorl <gutin22@gmail.com>
2021-07-02 19:18:18 -0300

    update: user list to axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,36 +2,15 @@
 
 import { Box, Button, Text, Flex, Heading, Table, Th, Thead, Tr, Checkbox, Icon, Tbody, Td, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import Link from 'next/link';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import Pagination from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
-import { useQuery } from 'react-query';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error} = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users');
-        const data = await response.json();
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            }
-        }, {
-            staleTime: 1000 * 5,
-        });
-
-        return users;
-    });
-
+    const { data, isLoading, isFetching, error} = useUsers()
 
     const isDesktop = useBreakpointValue({
         base: false,
