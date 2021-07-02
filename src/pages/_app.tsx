b435f416bbbeabeb32a1891c3eacc905c70fd8e6
augustorl <gutin22@gmail.com>
2021-07-01 23:57:29 -0300

    add: ReactQueryDevTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme';
+
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
@@ -20,6 +22,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools/>
     </QueryClientProvider>
 
   )
