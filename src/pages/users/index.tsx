85b9e5c251bf2cc13e291b5c24e1d6df70cdf0d7
augustorl <gutin22@gmail.com>
2021-07-05 23:54:00 -0300

    update: userList index

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,8 +10,8 @@ import { Sidebar } from '../../components/Sidebar';
 import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error} = useUsers();
     const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error} = useUsers(page);
     const isDesktop = useBreakpointValue({
         base: false,
         lg: true,
@@ -67,7 +67,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
@@ -88,7 +88,7 @@ export default function UserList() {
                                 </Tbody>
                             </Table>
                             <Pagination 
-                                totalCountOfRegisters={100}
+                                totalCountOfRegisters={data.totalCount}
                                 currentPage={page}
                                 onPageChange={setPage}
                             />
